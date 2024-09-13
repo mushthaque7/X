352f0afdc522dc88f72aa34453a8e1843ea58c52
mushthaque <musthaquemt7@gmail.com>
2024-09-13 19:51:59 +0530

    Auth controller added

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,16 @@ import express from "express";
 import authRotes from "./routes/auth.routes.js";
 import dotenv from "dotenv";
 import connectDB from "./db/connect.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config()
+
 const app= express()
 const port=process.env.PORT || 5000;
 
+app.use(express.json())
+app.use(express.urlencoded({extended:true})) //to parse form data
+app.use(cookieParser())
 app.use("/api/auth",authRotes)
 
 
